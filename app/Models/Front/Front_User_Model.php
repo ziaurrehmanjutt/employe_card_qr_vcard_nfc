82c4230adf965eb246769f504037c23ba4178511
ZIA UR REHMAN <ziaa520@gmail.com>
2022-12-17 11:58:26 +0500

    add fronts pages

--- app/Models/Front/Front_User_Model.php
+++ app/Models/Front/Front_User_Model.php
@@ -5,7 +5,6 @@ use App\Models\Front\Front_Base_Model;
 class Front_User_Model extends Front_Base_Model
 {
     public function login($email){
-
         $builder =  $this->db->table('all_users');
         $builder->select('*')->where('user_email',$email);
         $result = $builder->get()->getRowArray();
